ad5ccbe108e9dfff13e2028b21ed7c9650756995
Bart Saelen <bart.saelen@geosolutions.be>
2020-05-26 16:54:04 +0200

    add harvester info

--- ckanext/benapharvest/harvesters/ckan_benap_harvester.py
+++ ckanext/benapharvest/harvesters/ckan_benap_harvester.py
@@ -1,16 +1,24 @@
 from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
 
+
 class CkanBenapHarvester(CKANHarvester):
 
-    def modify_package_dict(self, package_dict, harvest_object):
+    def info(self):
+        return {
+            'name': 'benap_ckan',
+            'title': 'BENAP CKAN',
+            'description': 'Harvests remote CKAN instances for use with transportdata.be',
+            'form_config_interface': 'Text'
+        }
 
+    def modify_package_dict(self, package_dict, harvest_object):
         # Set a default custom field
 
-        print("#"*30)
+        print("#" * 30)
         print(package_dict)
-        print("#"*30)
+        print("#" * 30)
         print(harvest_object)
-        print("#"*30)
+        print("#" * 30)
 
         package_dict['remote_harvest'] = True
 
