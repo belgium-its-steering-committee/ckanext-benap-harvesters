d886680c970c1ec1e72b31093ba1ff453fd647aa
Bart Saelen <bart.saelen@geosolutions.be>
2020-05-28 16:17:58 +0200

    add more debugging

--- ckanext/benapharvest/harvesters/dcat_benap_harvester.py
+++ ckanext/benapharvest/harvesters/dcat_benap_harvester.py
@@ -16,6 +16,10 @@ class DcatBenapHarvester(DCATHarvester):
         }
 
     def gather_stage(self, harvest_job):
+        log.debug(harvest_job)
+        log.debug(self)
+        log.debug(super(DcatBenapHarvester, self))
+        log.debug(super(DcatBenapHarvester, self).gather_stage)
         super(DcatBenapHarvester, self).gather_stage(harvest_job)
 
     def modify_package_dict(self, package_dict, dcat_dict, harvest_object):
