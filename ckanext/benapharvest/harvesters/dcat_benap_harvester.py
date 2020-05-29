0190f6065e35f5b6654422aa1962df6a86d20467
Bart Saelen <bart.saelen@geosolutions.be>
2020-05-29 10:22:33 +0200

    add extra mappings

--- ckanext/benapharvest/harvesters/dcat_benap_harvester.py
+++ ckanext/benapharvest/harvesters/dcat_benap_harvester.py
@@ -20,74 +20,73 @@ class DcatBenapHarvester(DCATRDFHarvester):
 
     def modify_package_dict(self, package_dict, dcat_dict, harvest_object):
         log.debug("---modify_package_dict---")
-        log.debug("package_dict")
-        log.debug(json.dumps(package_dict, indent=2))
-
         log.debug('config')
         log.debug(harvest_object.source.config)
+        log.debug("-------")
 
-        package_dict['extras'] = self._process_extras(package_dict['extras'])
+        extras_keys = [val['key'] for val in package_dict['extras']]
 
         package_dict['remote_harvest'] = True
 
         # Add tags
         package_dict['tags'].append({'name': 'harvested'})
+        if 'fluent_tags' not in package_dict:
+            package_dict['fluent_tags'] = []
+        package_dict['fluent_tags'].append({'name': 'Cycle'})
 
+        # License
+        package_dict['license_id'] = "CC0-1.0"
+        package_dict['contract_license'] = "nolinoco"
+
+        # Geographic location
         package_dict['countries_covered'] = ['http://publications.europa.eu/resource/authority/country/BEL']
-        package_dict['regions_covered'] = ['http://publications.europa.eu/resource/authority/nuts/code/BE3']
-        package_dict['language'] = ['http://publications.europa.eu/resource/authority/language/NLD']
+        package_dict['regions_covered'] = ['http://data.europa.eu/nuts/code/BE3']
+
+        # Frequency
+        package_dict['frequency'] = 'http://publications.europa.eu/resource/authority/frequency/MONTHLY'
+
+        # Language
+
+        if 'Language' in extras_keys or 'language' in extras_keys:
+            new_languages = []
+            languages = json.loads(self._find_by_key(package_dict['extras'], 'language'))
+            if languages is None:
+                languages = json.loads(self._find_by_key(package_dict['extras'], 'Language'))
+            for language in languages:
+                new_languages.append(self._format_language(language))
+            package_dict['language'] = new_languages
+        else:
+            package_dict['language'] = ['http://publications.europa.eu/resource/authority/language/NLD']
+
+        # Theme
         package_dict['theme'] = 'http://publications.europa.eu/resource/authority/data-theme/TRAN'
+
+        # Temporal start
+        if 'Temporal start' in extras_keys:
+            package_dict['temporal_start'] = self._find_by_key(package_dict['extras'], 'Temporal start')
+        elif 'modified' in extras_keys:
+            package_dict['temporal_start'] = self._find_by_key(package_dict['extras'], 'modified')
+        else:
+            now = datetime.now()
+            package_dict['temporal_start'] = now.strftime("%Y-%m-%dT%H:%M:%S")
+
+        # Publisher
+        package_dict['p_address'] = 'publisher avenue, 1000 Bruxelles (Belgique)'
+        package_dict['p_tel'] = '+32488999999'
+        package_dict['publisher_org'] = 'new publisher'
+        package_dict['publisher_name'] = 'publisher name'
+        package_dict['publisher_url'] = 'https://www.publisher.com'
+        package_dict['publisher_email'] = 'publisher@example.com'
+
+        # Contact
+        package_dict['contact_name'] = 'contact point name'
+
+        # Other
+        package_dict['cont_res'] = 'Cont Res'
 
         log.debug("final package_dict")
         log.debug(json.dumps(package_dict, indent=2))
         return package_dict
-
-    def _process_extras(self, extras):
-        extras_keys = [val['key'] for val in extras]
-
-        new_extras = []
-
-        # Temporal start
-        if 'Temporal start' in extras_keys:
-            self._add_to_dict_list(new_extras, 'Temporal start', self._find_by_key(extras, 'Temporal start'))
-        elif 'modified' in extras_keys:
-            self._add_to_dict_list(new_extras, 'Temporal start', self._find_by_key(extras, 'modified'))
-        else:
-            now = datetime.now()
-            self._add_to_dict_list(new_extras, 'Temporal start', now.strftime("%Y-%m-%dT%H:%M:%S"))
-
-        # Regions covered
-        if 'Regions covered' in extras_keys:
-            self._add_to_dict_list(new_extras, 'Regions covered', self._find_by_key(extras, 'Regions covered'))
-        else:
-            self._add_to_dict_list(new_extras, 'Regions covered',
-                                   ['http://publications.europa.eu/resource/authority/nuts/code/BE3'])
-
-        self._add_to_dict_list(new_extras, 'regions_covered',
-                               ['http://publications.europa.eu/resource/authority/nuts/code/BE3'])
-
-        # Countries covered
-        if 'Countries covered' in extras_keys:
-            self._add_to_dict_list(new_extras, 'Countries covered', self._find_by_key(extras, 'Countries covered'))
-        else:
-            self._add_to_dict_list(new_extras, 'Countries covered',
-                                   ['http://publications.europa.eu/resource/authority/country/BEL'])
-
-        # Language
-        if 'Language' in extras_keys or 'language' in extras_keys:
-            new_languages = []
-            languages = json.loads(self._find_by_key(extras, 'language'))
-            if languages is None:
-                languages = json.loads(self._find_by_key(extras, 'Language'))
-            for language in languages:
-                new_languages.append(self._format_language(language))
-            self._add_to_dict_list(new_extras, 'Language', new_languages)
-        else:
-            self._add_to_dict_list(new_extras, 'Language',
-                                   ['http://publications.europa.eu/resource/authority/language/NLD'])
-
-        log.debug(json.dumps(new_extras, indent=2))
-        return new_extras
 
     @staticmethod
     def _format_language(language):
@@ -102,6 +101,6 @@ class DcatBenapHarvester(DCATRDFHarvester):
     @staticmethod
     def _add_to_dict_list(dict_list, key, value):
         dict_list.append({
-            "key":key,
-            "value":value
+            "key": key,
+            "value": value
         })
