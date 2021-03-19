1e079573304e8e90c98c3a786c9491eb0935bebd
Bart Saelen <bart.saelen@geosolutions.be>
2021-03-19 17:46:52 +0100

    fluent_tags

--- ckanext/benapharvest/harvesters/dcat_simple_harvester.py
+++ ckanext/benapharvest/harvesters/dcat_simple_harvester.py
@@ -33,11 +33,11 @@ class DcatSimpleHarvester(DCATRDFHarvester):
         log.debug(package_dict['tags'])
         log.debug(tags)
         log.debug("---")
+        log.debug(package_dict)
+        log.debug("---")
         #fluent_tags = [fluent_tag_object_from_tag_value(val) for val in tags]
         #log.debug(fluent_tags)
         package_dict['fluent_tags'] = tags
-        log.debug("---")
-        log.debug(package_dict)
         log.debug("---")
 
 
